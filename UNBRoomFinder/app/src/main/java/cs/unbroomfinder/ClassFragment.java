43dfd9737ef5827690976d922953e4c746e706c1
Caleb Reath <Caleb Reath>
2017-02-19 16:29:48 -0400

    organizer - Added instance of Add Class button to class-tasb fragment

--- UNBRoomFinder/app/src/main/java/cs/unbroomfinder/ClassFragment.java
+++ UNBRoomFinder/app/src/main/java/cs/unbroomfinder/ClassFragment.java
@@ -4,19 +4,32 @@ package cs.unbroomfinder;
  * Created by kylebrideau on 2017-02-18.
  */
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class ClassFragment extends Fragment {
+public class ClassFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_class, container, false);
+
+        Button btn = (Button) rootView.findViewById(R.id.btn_add_class);
+        btn.setOnClickListener(this);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), AddClassActivity.class);
+        startActivity(intent);
     }
 }
