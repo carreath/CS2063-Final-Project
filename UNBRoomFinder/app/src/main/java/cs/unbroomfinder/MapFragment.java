a221101b2b79249bd5a617c5f0b403ec6dccb150
Kyle Brideau <kylebrideau@192.168.2.21>
2017-02-19 14:37:36 -0400

    New code base - with layout started

---
+++ UNBRoomFinder/app/src/main/java/cs/unbroomfinder/MapFragment.java
@@ -0,0 +1,22 @@
+package cs.unbroomfinder;
+
+/**
+ * Created by kylebrideau on 2017-02-18.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_map, container, false);;
+        return rootView;
+    }
+}
