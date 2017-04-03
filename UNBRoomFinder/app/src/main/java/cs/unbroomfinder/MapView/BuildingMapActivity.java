b06153d90637eb2abc72cf978f8c8b405872dfb9
Caleb Reath <Caleb Reath>
2017-04-03 16:53:35 -0300

    MapView - Added transparent paths for seperate levels

--- UNBRoomFinder/app/src/main/java/cs/unbroomfinder/MapView/BuildingMapActivity.java
+++ UNBRoomFinder/app/src/main/java/cs/unbroomfinder/MapView/BuildingMapActivity.java
@@ -145,27 +145,49 @@ public class BuildingMapActivity extends AppCompatActivity {
             if(mBitmap != null) canvas.drawBitmap(mBitmap, 0,0, paint);
 
             if(shortestPath != null) {
-                for(int k=1; k<5; k*=4) {
-                    if(k==1) paint.setColor(Color.parseColor("#609dff"));
-                    HashMap<Integer, GraphNode> currentFloor = map.graph.floors[selectedImage];
-                    paint.setStrokeWidth(2 * (PATH_RADIUS + k));
-                    paint.setStrokeCap(Paint.Cap.ROUND);
+                Bitmap bmOverlay = Bitmap.createBitmap(mBitmap.getWidth(), mBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvasOverlay = new Canvas(bmOverlay);
 
-                    PriorityNode current = shortestPath;
-                    while (current != null) {
-                        if (current.prev == null) break;
-                        if (currentFloor.containsKey(current.cur.id) && currentFloor.containsKey(current.prev.cur.id)) {
-                            canvas.drawLine(current.cur.coords.x, current.cur.coords.y, current.prev.cur.coords.x, current.prev.cur.coords.y, paint);
-                        }
-                        current = current.prev;
+                HashMap<Integer, GraphNode> currentFloor = map.graph.floors[selectedImage];
+
+                PriorityNode current = shortestPath;
+                while (current != null) {
+                    if (current.prev == null) break;
+
+                    if(currentFloor.containsKey(current.cur.id) && currentFloor.containsKey(current.prev.cur.id)) {
+                        drawLine(canvas, current.cur.coords.x, current.cur.coords.y, current.prev.cur.coords.x, current.prev.cur.coords.y);
                     }
+                    else {
+                        drawLine(canvasOverlay, current.cur.coords.x, current.cur.coords.y, current.prev.cur.coords.x, current.prev.cur.coords.y);
+                    }
+                    current = current.prev;
                 }
+
+                paint.setColor(Color.parseColor("#50FFFFFF"));
+                canvas.drawBitmap(bmOverlay, 0,0, paint);
             }
             if(endNode != null && 0 == 1) {
                 canvas.drawCircle(endNode.x, endNode.y, PATH_RADIUS, paint);
             }
 
             canvas.restore();
+        }
+
+        public void drawLine(Canvas canvas, int x1, int y1, int x2, int y2) {
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setDither(true);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            paint.setColor(Color.parseColor("#3d87ff"));
+
+            paint.setStrokeWidth(2 * (PATH_RADIUS + 4));
+            paint.setColor(Color.parseColor("#609dff"));
+            canvas.drawLine(x1,y1,x2,y2, paint);
+
+            paint.setStrokeWidth(2 * PATH_RADIUS);
+            canvas.drawLine(x1,y1,x2,y2, paint);
         }
 
         float grabbedX = 0, grabbedY = 0;
